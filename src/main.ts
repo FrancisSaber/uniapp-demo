fc021feb81c660264ac9e13a5a92c69bb8253ffd
ccy <13692026228@163.com>
2024-04-23 03:15:01 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createSSRApp } from "vue";
+import App from "./App.vue";
+import uviewPlus, { setConfig } from 'uview-plus'
+export function createApp() {
+  const app = createSSRApp(App);
+  // main.js，注意要在use方法之后执行
+
+  app.use(uviewPlus)
+  return {
+    app,
+  };
+}
